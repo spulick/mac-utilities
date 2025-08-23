5aec3e916dc392aceb2361db9a3242aaa6426899
Sreehari Pulickamadhom Sreedhar <orectique@gmail.com>
2025-08-24 00:50:41 +1000

    send files over :)

---
+++ hammerspoon/init.lua
@@ -0,0 +1,35 @@
+-- A very simple bongocat. Can always replace the left and right images.
+
+bongocat = hs.menubar.new()
+
+local eventtap = require("hs.eventtap")
+local image = require("hs.image")
+local events = eventtap.event.types
+
+local left_cat = image.imageFromPath("./Assets/left.png"):setSize({w = 40, h = 40})
+local right_cat = image.imageFromPath("./Assets/right.png"):setSize({w = 40, h = 40})
+
+if bongocat then
+    hs.eventtap.new({ events.keyUp }, function()
+        local currentIcon = bongocat:icon()
+        if currentIcon == left_cat then
+            bongocat:setIcon(right_cat)
+        else
+            bongocat:setIcon(left_cat)
+        end
+    end):start()
+else
+    hs.alert.show("Failed to create Bongocat menubar item")
+end
+
+-- Automatically tile Stata windows to the noice configuration.
+
+function stataWatcher(appName, eventType, appObject)
+    if (eventType == hs.application.watcher.activated) and (appName == "StataNow/MP 19.5") then
+        appObject:selectMenuItem({"Window", "Fill"})
+
+    end
+end
+
+stataWatcherInstance = hs.application.watcher.new(stataWatcher)
+stataWatcherInstance:start()
---
+++ src_cp.sh
@@ -0,0 +1,1 @@
+cp -R ~/.hammerspoon ./hammerspoon # bring the hammerspoon files over
